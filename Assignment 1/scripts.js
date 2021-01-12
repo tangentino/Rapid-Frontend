5a0edc2bcd808b44ecd36a0ed964736a881be542
Tan Kongpithaksilp <45220855+tangentino@users.noreply.github.com>
2021-01-12 14:24:07 +0700

    Update scripts.js

--- Assignment 1/scripts.js
+++ Assignment 1/scripts.js
@@ -1,3 +1,5 @@
+var valid = false;
+
 function validateEmail() {
     var email = document.getElementById("email-box");
     var error;
@@ -5,20 +7,21 @@ function validateEmail() {
         error = "";
         email.style.borderColor = "#aaa";
         email.style.boxShadow = "0 0 0 2pt #c8e1ff";
+        valid = true;
     }
     else {
         error = "A valid email is required";
         email.style.borderColor = "red";
         email.style.boxShadow = "0 0 0 2pt #f7d0d5"
+        valid = false;
     }
     document.getElementById("error-message").innerHTML = error;
 }
 
 function submit() {
-    var email = document.getElementById("email-box");
     var mainPage = document.getElementById("main-div");
     var successPage = document.getElementById("success");
-    if (email.checkValidity()) {
+    if (valid) {
         mainPage.style.display = "none";
         successPage.style.display = "block";
     }
